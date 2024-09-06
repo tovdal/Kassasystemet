2865c83495df4b023d10eca900d0982d670e5c95
tovdal <tova.dalin@gmail.com>
2024-09-06 14:22:19 +0200

    Basic switch menu

--- Kassasystemet/Program.cs
+++ Kassasystemet/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Cashier System 1.0\n");
+
+            Console.WriteLine("[1] New Costumer");
+            Console.WriteLine("[2] Admin tools");
+            Console.WriteLine("[3] Exit\n");
+
+            Console.Write("Pick selection: ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("[1] New Costumer");
+                    break;
+                case 2:
+                    Console.WriteLine("[2] Admin tools");
+                    switch (num)
+                    {
+                        case 1:
+                            Console.WriteLine("[1] Change product information");
+                            // Här inne kommer det att ligga information om produkter. Man ska kunda ändra produkterna eller tabort dem / addera nya!
+
+                            break;
+                        case 2:
+                            Console.WriteLine("[2] Change deals and promotional prices");
+                            // här inne kommer det att ligga information om kampanjpriser
+                            // "Det som menas är att tex från 2023-03-18 till 2023-03-19 kostar mjölken 10 kr"
+                            // Man ska kunna tabort och lägga till
+                            // kampanjpriserna måste var med på kvitto!!!
+                            break;
+                        case 3:
+                            Console.WriteLine("[3] Return");
+                            break;
+                    }
+                    break;
+                case 3:
+                    Console.WriteLine("[3] Exit");
+                    break;
+
+            }
+
         }
     }
 }
