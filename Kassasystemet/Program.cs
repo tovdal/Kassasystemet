600c3c52e796d73fd57cd935a3141b2a8ee36523
tovdal <tova.dalin@gmail.com>
2024-09-10 14:56:40 +0200

    Split() added to new customer

--- Kassasystemet/Program.cs
+++ Kassasystemet/Program.cs
@@ -19,6 +19,28 @@
             {
                 case 1:
                     Console.WriteLine("[1] New Costumer");
+                    // det man skriver in här ska vara en produkt kod och antal av produkten.
+                    // Sedan ska dessa sparas och visas i konsollen.
+                    Console.Clear();
+                    DateTime now = DateTime.Now;
+                    Console.WriteLine("cash register");
+                    Console.Write($"RECEIPT      ");
+                    Console.Write(now.ToString("yyyy-MM-dd HH:mm:ss"));
+
+                    // här kommer det att vara de produkter som skrivs in och sparas.
+
+                    Console.WriteLine("comands: ");
+                    Console.WriteLine("<product id> <amount> \n PAY");
+                    Console.Write("comand:");
+                    // Varor skrivs in och splitras i artikel och antal
+                    string userComand = Console.ReadLine();
+                    string[] comands = userComand.Split(' ');
+                    Console.WriteLine(comands[0]);
+                    Console.WriteLine(comands[1]);
+
+                    //PAY ska räkna ihop allt till ett kvitto och slut summa av köpet.
+                    // exempel moms, total kostnad, kanske även hur många produkter man köpte totalt
+
                     break;
                 case 2:
                     Console.WriteLine("[2] Admin tools");
@@ -35,6 +57,15 @@
                             // "Det som menas är att tex från 2023-03-18 till 2023-03-19 kostar mjölken 10 kr"
                             // Man ska kunna tabort och lägga till
                             // kampanjpriserna måste var med på kvitto!!!
+
+                            //ToChar
+                            Console.WriteLine("Vilken produkt letar du efter?");
+                            string imput = Console.ReadLine();
+                            ushort shortSearch = Convert.ToUInt16(Console.ReadLine()[0]);
+
+                            // Fortsätt vidare senare
+
+
                             break;
                         case 3:
                             Console.WriteLine("[3] Return");
