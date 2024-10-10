ad7cf723eb1f0d55c769c5798d33ceb6d81032f9
Tovdal <tova.dalin@hotmail.com>
2024-10-10 13:28:23 +0200

    Add more information and consoleCenter.CenterText

--- Kassasystemet/Kassasystemet/HandleCustomer.cs
+++ Kassasystemet/Kassasystemet/HandleCustomer.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Win32;
+﻿using Kassasystemet.VisualChanges;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Kassasystemet.Kassasystemet
 {
@@ -13,26 +16,38 @@ namespace Kassasystemet.Kassasystemet
         {
             // hanterar kund
             string productFilePath = "../../../Files/products.txt"; //Filvägen till produkterna
+
             CashRegister register = new CashRegister(productFilePath);
+            Receipt receipt = new Receipt();
             List<Product> shoppingCart = new List<Product>();
+            ConsoleCenter consoleCenter = new ConsoleCenter();
+
             string input;
             bool IsPaymentCompleted = false;
 
             do
             {
                 Console.Clear();
-                Console.WriteLine("Cash Registre");
-                Console.WriteLine($"Receipt     {DateTime.Now:yyyy-MM-dd HH:mm:ss}\n");
+
+                int lines = 20;
+                consoleCenter.SetCursorToMiddle(lines);
+
+                consoleCenter.CenterText("───────────────────────────────────");
+
+                consoleCenter.CenterText("Cash Register\n");
+                consoleCenter.CenterText($"Receipt     {DateTime.Now:yyyy-MM-dd HH:mm:ss}\n");
 
                 foreach (var products in shoppingCart)
                 {
-                    Console.WriteLine($"{products.ProductName} - {products.Price:C}");
+                    consoleCenter.CenterText($"{products.ProductName} - {products.Price:C}");
                 }
 
-                Console.WriteLine($"Total: {Receipt.CalculateTotal(shoppingCart):C}");
-                Console.WriteLine("<PLU code> or type 'PAY' to complete");
-                Console.WriteLine("Command: ");
+                consoleCenter.CenterText($"Total: {Receipt.CalculateTotal(shoppingCart):C}");
+                consoleCenter.CenterText("<PLU code> or type 'PAY' to complete");
+                consoleCenter.CenterText("Command: ");
+                consoleCenter.CenterText("───────────────────────────────────");
                 input = Console.ReadLine();
+
 
                 if (input.ToUpper() != "PAY")
                 {
@@ -52,12 +67,15 @@ namespace Kassasystemet.Kassasystemet
                         }
                         else
                         {
-                            Console.WriteLine("Product not found.");
+                            consoleCenter.CenterText("Product not found.");
+                            Console.ReadKey();
                         }
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine($"Error: {e.Message}");
+                        consoleCenter.CenterText($"Error: {e.Message}");
+                        consoleCenter.CenterText("Press any key to continue");
+                        Console.ReadKey();
                     }
                 }
                 else
@@ -66,7 +84,11 @@ namespace Kassasystemet.Kassasystemet
                 }
             }
             while (!IsPaymentCompleted);
-            Receipt.SaveReceipt(shoppingCart); //När betalningen är klart, kvittot sparas.
+            receipt.SaveReceipt(shoppingCart); //När betalningen är klart, kvittot sparas.
+
+            consoleCenter.CenterText("Receipt saved and printed out.");
+            consoleCenter.CenterText("Press any key to continue");
+            Console.ReadKey();
         }
     }
 }
