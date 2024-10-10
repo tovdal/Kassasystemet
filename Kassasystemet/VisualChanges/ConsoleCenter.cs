b9061901dd206615eb108c22a6b8cb0716eb7aac
Tovdal <tova.dalin@hotmail.com>
2024-10-10 11:33:51 +0200

    Add center text in console application

---
+++ Kassasystemet/VisualChanges/ConsoleCenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.VisualChanges
+{
+    public class ConsoleCenter
+    {
+        public void CenterText(string text)
+        {
+            int windowWidth = Console.WindowWidth;
+            int textLength = text.Length;
+            int space = (windowWidth - textLength) / 2;
+
+            Console.WriteLine(new string(' ', space) + text);
+        }
+        public void SetCursorToMiddle(int lines)
+        {
+            int windowHeight = Console.WindowHeight;
+            int verticalPosition = (windowHeight - lines) / 2;
+            Console.SetCursorPosition(0, verticalPosition);
+        }
+
+    }
+}
