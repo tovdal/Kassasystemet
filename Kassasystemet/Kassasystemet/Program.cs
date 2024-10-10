5559ede46eaf46415514602fc635584454c7f827
Tovdal <tova.dalin@hotmail.com>
2024-10-10 10:01:38 +0200

    add switch menu

--- Kassasystemet/Kassasystemet/Program.cs
+++ Kassasystemet/Kassasystemet/Program.cs
@@ -10,20 +10,50 @@ namespace Kassasystemet.Kassasystemet
         static void Main(string[] args)
         {
             string productFilePath = "../../../Files/products.txt"; //Filvägen till produkterna
-            
-            //Console.WriteLine("Cash Register System");
+            CashRegister register = new CashRegister(productFilePath);
+            bool IsRunning = true;
 
-            //// Initialisera CashRegister och andra klasser
-            //var cashRegister = new CashRegister("../../../Products.txt");
+            while (IsRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Cashier System 1.0\n");
+                Console.WriteLine("[1] New Customer");
+                Console.WriteLine("[2] Admin Tools");
+                Console.WriteLine("[3] Exit\n");
 
-            //cashRegister.AddToCart(101, 2); // Exempel på PLU-kod och kvantitet
-            //cashRegister.AddToCart(102, 3); // Exempel på PLU-kod och kvantitet
-            //cashRegister.AddToCart(999, 1); // En ogiltig kod för att se felmeddelande
+                Console.WriteLine("Pick selection: ");
+                string choice = Console.ReadLine();
 
-            //// Visa totalbeloppet
-            //decimal total = cashRegister.GetTotalAmount();
-            //Console.WriteLine($"Total amount in cart: {total:C}");
-            //// Logik för användarinteraktion
+                switch (choice)
+                {
+                    case "1":
+                        // hanterar kund
+                        break;
+                    case "2":
+                        // Admin verktyg? om jag kommer så långt,
+                        break;
+                    case "3":
+                        IsRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+
+                //Console.WriteLine("Cash Register System");
+
+                //// Initialisera CashRegister och andra klasser
+                //var cashRegister = new CashRegister("../../../Products.txt");
+
+                //cashRegister.AddToCart(101, 2); // Exempel på PLU-kod och kvantitet
+                //cashRegister.AddToCart(102, 3); // Exempel på PLU-kod och kvantitet
+                //cashRegister.AddToCart(999, 1); // En ogiltig kod för att se felmeddelande
+
+                //// Visa totalbeloppet
+                //decimal total = cashRegister.GetTotalAmount();
+                //Console.WriteLine($"Total amount in cart: {total:C}");
+                //// Logik för användarinteraktion
+            }
         }
     }
 }
