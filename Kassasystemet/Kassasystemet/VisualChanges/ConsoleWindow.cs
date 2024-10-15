8f13c7359748c0f2fd9aadd845f734b7386f9bb6
Tovdal <tova.dalin@hotmail.com>
2024-10-15 15:48:19 +0200

    Add ConsoleWindow Class to resize the console window.

---
+++ Kassasystemet/Kassasystemet/VisualChanges/ConsoleWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Kassasystemet.VisualChanges
+{
+    public class ConsoleWindow
+    {
+        public void WindowSize() 
+        {
+            Console.SetWindowSize(200, 50);
+        }
+    }
+}
