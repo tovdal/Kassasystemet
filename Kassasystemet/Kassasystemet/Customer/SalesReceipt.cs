1613e737bfaf12bf320a62d31aa0edd449e5c11b
Tovdal <tova.dalin@hotmail.com>
2024-10-15 14:30:55 +0200

    Added Calculate Tax and some improvments to the SaveReceipt

--- Kassasystemet/Kassasystemet/Customer/SalesReceipt.cs
+++ Kassasystemet/Kassasystemet/Customer/SalesReceipt.cs
@@ -10,14 +10,27 @@ namespace Kassasystemet.Kassasystemet.Customer
     // Hanterar produkter som köpts och ansvarar för att beräkna totalsumman samt skriva ut kvittot.
     public class SalesReceipt
     {
-        public static decimal CalculateTotal(List<Product> shoppingCart)
+        public decimal CalculateTotal(List<Product> shoppingCart)
         {
             decimal total = 0;
+
             foreach (var product in shoppingCart)
             {
                 total += product.Price;
             }
             return total;
+        }
+        public decimal CalculateTax(List<Product> shoppingCart)
+        {
+            decimal tax = 0;
+
+            foreach (var product in shoppingCart)
+            {
+                tax += product.Price;
+            }
+            tax *= 0.12m; 
+
+            return tax;
         }
 
         public void SaveReceipt(List<Product> shoppingCart)
@@ -25,27 +38,39 @@ namespace Kassasystemet.Kassasystemet.Customer
             string receiptFilePath = $"../../../Files/RECEIPT_{DateTime.Now:yyyyMMdd}.txt";
             using (StreamWriter writer = new StreamWriter(receiptFilePath, append: true))
             {
-                writer.WriteLine("───────────────────────");
-                writer.WriteLine("======= RECEIPT =======");
-                writer.WriteLine("Super Market - NoWhere");
-                writer.WriteLine("No-Where Steet 0");
-                writer.WriteLine("234 56 No-Where");
-                writer.WriteLine("TEL NR 010-0000000");
-                writer.WriteLine($"Date: {DateTime.Now:yyyy-MM-dd HH:mm:ss}\n");
+                writer.WriteLine(" ───────────────────────────");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|========= RECEIPT =========|");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|Super Market - NoWhere     |");
+                writer.WriteLine("|No-Where Steet 0           |");
+                writer.WriteLine("|234 56 No-Where            |");
+                writer.WriteLine("|TEL NR 010-0000000         |");
+                writer.WriteLine($"|Date: {DateTime.Now:yyyy-MM-dd HH:mm:ss}  |");
+                writer.WriteLine("|---------------------------|");
+                writer.WriteLine("|                           |");
                 foreach (var product in shoppingCart)
                 {
-                    writer.WriteLine($"{product.ProductName} - {product.Price:C}");
+                    writer.WriteLine($"|{product.ProductName} - {product.Price:C}");
                 }
-                writer.WriteLine($"\n{shoppingCart.Count} : articles");
-                writer.WriteLine($"Total: {CalculateTotal(shoppingCart):C}\n");
-                writer.WriteLine("-----------------------");
-                writer.WriteLine("THANKS AND WELCOME");
-                writer.WriteLine("BACK TO THE SHOP\n");
-                writer.WriteLine("chashier 1 1122002");
-                writer.WriteLine("LÖPNUMMER ska vara här");
-                writer.WriteLine("****** ORIGINAL *******");
-                writer.WriteLine("=======================");
-                writer.WriteLine("───────────────────────");
+                writer.WriteLine("|                           |");
+                writer.WriteLine($"|Articles: {shoppingCart.Count}");
+                writer.WriteLine($"|Total: {CalculateTotal(shoppingCart):C}");
+                writer.WriteLine($"|Taxes: {CalculateTax(shoppingCart):C}");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|---------------------------|");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|    THANKS AND WELCOME     |");
+                writer.WriteLine("|     BACK TO THE SHOP      |");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|     cashier 1 1122002     |");
+                writer.WriteLine("|  LÖPNUMMER ska vara här   |");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|  ****** ORIGINAL *******  |");
+                writer.WriteLine("|                           |");
+                writer.WriteLine("|===========================|");
+                writer.WriteLine("|                           |");
+                writer.WriteLine(" ───────────────────────────");
                 writer.WriteLine("\n\n");
 
             }
