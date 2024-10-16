bc5b79532175a08d619ba5963ac2eada3ecd73ab
Tovdal <tova.dalin@hotmail.com>
2024-10-16 17:03:07 +0200

    Removed methods from NewCustomer Class

--- Kassasystemet/Customer/NewCustomer.cs
+++ Kassasystemet/Customer/NewCustomer.cs
@@ -28,14 +28,14 @@ namespace Kassasystemet.Customer
 
             do
             {
-                DisplayAvailableProducts(productManager);
-                DisplayReceipt(calculateReceipt, consoleCenter, shoppingCart);
+                DisplayProducts.DisplayAvailableProducts(productManager);
+                DisplayReceipt.DisplayCustomerReceipt(calculateReceipt, consoleCenter, shoppingCart);
 
                 input = Console.ReadLine();
 
                 if (input.ToUpper() != "PAY")
                 {
-                    HandleProductInput(consoleCenter, productManager, shoppingCart, input);
+                    CustomerImput.HandleProductInput(consoleCenter, productManager, shoppingCart, input);
                 }
                 else
                 {
@@ -51,73 +51,5 @@ namespace Kassasystemet.Customer
             Console.ReadKey();
         }
 
-        private static void DisplayReceipt(CalculateReceipt calculateReceipt, ConsoleWriteLineCenter consoleCenter, List<Product> shoppingCart)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            consoleCenter.CenterText("Cash Register - New Customer\n");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            consoleCenter.CenterText("─────────────────────────────────────────────────────");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Green;
-            consoleCenter.CenterText($"Receipt     {DateTime.Now:yyyy-MM-dd HH:mm:ss}\n");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            // visa shoppingCart varorna i 
-            foreach (var products in shoppingCart)
-            {
-                consoleCenter.CenterText($"{products.ProductName} - {products.Price:C}");
-            }
-            consoleCenter.CenterText("");
-            consoleCenter.CenterText($"Total:                 {calculateReceipt.CalculateTotal(shoppingCart):C}");
-            consoleCenter.CenterText($"Taxes:                 {calculateReceipt.CalculateTax(shoppingCart):C}");
-            consoleCenter.CenterText("                           ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            consoleCenter.CenterText("Commands: <PLU> <amount> or type 'PAY' to complete");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            consoleCenter.CenterText("─────────────────────────────────────────────────────");
-            consoleCenter.CenterTextShort("Command: ");
-        }
-
-        private static void HandleProductInput(ConsoleWriteLineCenter consoleCenter, ProductManager productManager, List<Product> shoppingCart, string input)
-        {
-            try
-            {
-                string[] parts = input.Split(' ');
-                int pluCode = int.Parse(parts[0]);
-                int amount = int.Parse(parts[1]);
-
-                Product product = productManager.GetProductByPLU(pluCode);
-                if (product != null)
-                {
-                    for (int i = 0; i < amount; i++)
-                    {
-                        shoppingCart.Add(product);
-                    }
-                }
-                else
-                {
-                    consoleCenter.CenterText("Product not found.");
-                    Console.ReadKey();
-                }
-            }
-            catch (Exception e)
-            {
-                consoleCenter.CenterText($"Error: {e.Message}");
-                consoleCenter.CenterText("Press any key to continue");
-                Console.ReadKey();
-
-            }
-        }
-
-        private static void DisplayAvailableProducts(ProductManager productManager)
-        {
-            Console.Clear();
-            Console.WriteLine("\n\t\t ______________________________");
-            Console.WriteLine("\t\t|Available Products:");
-            foreach (var product in productManager.GetProducts())
-            {
-                Console.WriteLine($"\t\t|PLU: {product.PLUCode} - {product.ProductName} - {product.Unit}");
-            }
-        }
     }
 }
