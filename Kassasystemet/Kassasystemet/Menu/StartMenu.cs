1b1538a4d37ad0690ff67028f9124a26cec58bec
Tovdal <tova.dalin@hotmail.com>
2024-10-16 16:23:52 +0200

    Changed name of folder

---
+++ Kassasystemet/Kassasystemet/Menu/StartMenu.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kassasystemet.Kassasystemet.Customer;
+using Kassasystemet.Kassasystemet.Receipt;
+using Kassasystemet.Kassasystemet.VisualChanges;
+
+namespace Kassasystemet.Kassasystemet.MenuSystem
+{
+    public class StartMenu
+    {
+        public void RunMenu()
+        {
+            var cashRegisterMenu = new StartMenuDisplay();
+            var consoleWindow = new ConsoleWindow();
+            var newCustomer = new NewCustomer();
+            var consoleCenter = new ConsoleWriteLineCenter();
+
+            consoleWindow.WindowSize();
+
+            bool IsRunning = true;
+
+            while (IsRunning)
+            {
+                cashRegisterMenu.PrintOutMenu();
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        // hanterar kund
+                        newCustomer.StartNewCustormer();
+                        break;
+
+                    case "2":
+                        // Admin verktyg? om jag kommer så långt,
+                        consoleCenter.CenterText("Admin tools under construction....");
+                        Console.ReadKey();
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        int lines = 20;
+                        consoleCenter.SetCursorToMiddle(lines);
+                        consoleCenter.CenterText("Closing down the system...");
+                        consoleCenter.CenterText("Thank you for using Cashier System 1.0!");
+                        Console.ReadKey();
+                        IsRunning = false;
+                        break;
+
+                    default:
+                        consoleCenter.CenterText("Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Kassasystemet/Kassasystemet/Menu/StartMenuDisplay.cs
@@ -0,0 +1,41 @@
+﻿using Kassasystemet.Kassasystemet.VisualChanges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Kassasystemet.MenuSystem
+{
+    public class StartMenuDisplay
+    {
+        public void PrintOutMenu()
+        {
+            var consoleCenter = new ConsoleWriteLineCenter();
+            var titleDisplay = new TitleDisplay();
+
+            Console.Clear();
+
+            titleDisplay.PrintTitle();
+
+            int lines = 8;
+            consoleCenter.SetCursorToMiddle(lines);
+
+            Console.WriteLine("\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+            consoleCenter.CenterText("-:Menu:-\n");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            consoleCenter.CenterText("[1] New Customer");
+            consoleCenter.CenterText("[2] Admin Tools ");
+            consoleCenter.CenterText("[3] Exit      \n");
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            string prompt = "Pick selection: ";
+            int spacesBeforeCursor = (Console.WindowWidth - prompt.Length) / 2;
+            Console.SetCursorPosition(spacesBeforeCursor, Console.CursorTop + 1);
+            Console.Write(prompt); // writes out
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
