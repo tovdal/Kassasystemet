79750ce118dea33edec6732e136409fd2e00866d
Tovdal <tova.dalin@hotmail.com>
2024-10-16 17:02:36 +0200

    Add DisplayProducts Class

---
+++ Kassasystemet/Customer/DisplayProducts.cs
@@ -0,0 +1,23 @@
+﻿using Kassasystemet.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Customer
+{
+    public class DisplayProducts
+    {
+        public static void DisplayAvailableProducts(ProductManager productManager)
+        {
+            Console.Clear();
+            Console.WriteLine("\n\t\t ______________________________");
+            Console.WriteLine("\t\t|Available Products:");
+            foreach (var product in productManager.GetProducts())
+            {
+                Console.WriteLine($"\t\t|PLU: {product.PLUCode} - {product.ProductName} - {product.Unit}");
+            }
+        }
+    }
+}
