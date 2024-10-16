7fba6d207afa9550b881ff6869f4a19f6e2f62e6
Tovdal <tova.dalin@hotmail.com>
2024-10-16 16:25:47 +0200

    Add Class App that runs the program

---
+++ Kassasystemet/Kassasystemet/App.cs
@@ -0,0 +1,19 @@
+﻿using Kassasystemet.Kassasystemet.MenuSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Kassasystemet
+{
+    internal class App
+    {
+        public void Run()
+        {
+            var run = new StartMenu();
+            run.RunMenu();
+        }
+
+    }
+}
--- Kassasystemet/Kassasystemet/Program.cs
+++ Kassasystemet/Kassasystemet/Program.cs
@@ -1,29 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
-using Kassasystemet.Kassasystemet.Customer;
-using Kassasystemet.Kassasystemet.MenuSystem;
-using Kassasystemet.Kassasystemet.Receipt;
-using Kassasystemet.Kassasystemet.VisualChanges;
-
-
-namespace Kassasystemet.Kassasystemet
+﻿namespace Kassasystemet.Kassasystemet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var consoleWindow = new ConsoleWindow();
-            var calculateReceipt = new CalculateReceipt();
-            var salesReceipt = new PrintSalesReceipt();
-            var newCustomer = new NewCustomer();
-            var consoleCenter = new ConsoleCenter();
-            var titleDisplay = new TitleDisplay();
-            var latestReceiptNumber = new LatestReceiptNumber();
-
-            var startRegister = new StartMenu();
-            startRegister.RunMenu(consoleWindow, calculateReceipt, salesReceipt, newCustomer, consoleCenter, titleDisplay, latestReceiptNumber);
+            var run = new App();
+            run.Run();
         }
     }
 }
