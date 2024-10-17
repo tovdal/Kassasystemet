b145b76ff82806be4cdb74c123c1d40330a3487a
Tovdal <tova.dalin@hotmail.com>
2024-10-17 18:56:54 +0200

    Add new Class CartUniqProducts to gather same products in CartDisplay and Receipt

---
+++ Kassasystemet/Customer/CartUniqProducts.cs
@@ -0,0 +1,45 @@
+﻿using Kassasystemet.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Customer
+{
+    public class CartUniqProducts
+    {
+        public static List<Product> GatherProducts(List<Product> shoppingCart)
+        {
+            List<Product> uniqueProducts = new List<Product>();
+
+            foreach (var product in shoppingCart)
+            {
+                bool found = false;
+                foreach (var uniqueProduct in uniqueProducts)
+                {
+                    if (uniqueProduct.ProductName == product.ProductName)
+                    {
+                        uniqueProduct.Quantity += 1;
+                        uniqueProduct.Price = product.Price;
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    uniqueProducts.Add(new Product(
+                        product.PLUCode,
+                        product.ProductName,
+                        product.Price,
+                        product.Unit
+                    ));
+                }
+            }
+
+            return uniqueProducts;
+        }
+    }
+}
+
+
