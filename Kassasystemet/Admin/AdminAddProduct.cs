2e612be8bb5f71af18e258415411dd32bba079d9
Tovdal <tova.dalin@hotmail.com>
2024-10-18 17:25:34 +0200

    Add class AddProducts. User can now add new products.

---
+++ Kassasystemet/Admin/AdminAddProduct.cs
@@ -0,0 +1,43 @@
+﻿using Kassasystemet.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Admin
+{
+    public class AdminAddProduct
+    {
+        public void SaveNewProductToFile(ProductManager productManager, string filePath)
+        {
+            Console.WriteLine("Enter the PLU of the product: ");
+            int PLUCode = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the name of the product: ");
+            string productName = Console.ReadLine();
+            Console.WriteLine("Enter the price: ");
+            if (!int.TryParse(Console.ReadLine(), out int price))
+            {
+                Console.WriteLine("invalid price. Please enter a valid number.");
+                return;
+            }
+            Console.WriteLine("Is the product in kg or pc? (Enter 'kg' or 'pc')");
+            string unitInput = Console.ReadLine();
+            UnitType unit;
+
+            if (!Enum.TryParse(unitInput, true, out unit))
+            {
+                Console.WriteLine("Invalid unit type. Please enter either 'kg' or 'pc'.");
+                return;
+            }
+
+            Product product = new Product(PLUCode, productName, price, unit);
+
+            productManager.SaveNewProductToFile(filePath);
+
+            Console.WriteLine("Product added successfully.");
+            Console.ReadKey();
+
+        }
+    }
+}
