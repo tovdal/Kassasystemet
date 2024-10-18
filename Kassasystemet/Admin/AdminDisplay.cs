669156eb22b0f9ff7262dfa29da1e092962dc321
Tovdal <tova.dalin@hotmail.com>
2024-10-18 11:35:16 +0200

    Add admin display

---
+++ Kassasystemet/Admin/AdminDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Admin
+{
+    public class AdminDisplay
+    {
+        public void DisplayAdminChoices()
+        {
+
+        }
+    }
+}
