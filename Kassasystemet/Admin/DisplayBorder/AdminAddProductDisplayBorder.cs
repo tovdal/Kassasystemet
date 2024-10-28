b4a3abcab8a95e4998c9147cf9ec3583863a0824
Tovdal <tova.dalin@hotmail.com>
2024-10-28 13:56:47 +0100

    Add displayBorder to add admin product

---
+++ Kassasystemet/Admin/DisplayBorder/AdminAddProductDisplayBorder.cs
@@ -0,0 +1,19 @@
+﻿using Kassasystemet.VisualChanges;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kassasystemet.Admin.Display
+{
+    internal class AdminAddProductDisplayBorder
+    {
+        public void AdminAddProductBorder()
+        {
+            var consoleCenter = new ConsoleWriteLineCenter();
+            consoleCenter.DrawBorder(15, 74, 54, 12);
+        }
+
+    }
+}
