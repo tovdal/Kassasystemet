43d1b6e6ac25793ee8ed94d1fd2458bee0ea72da
Tovdal <tova.dalin@hotmail.com>
2024-10-28 16:07:01 +0100

    Moved customerborder

--- Kassasystemet/Customer/DisplayBorder/CustomerBorderDisplay.cs
+++ Kassasystemet/Customer/DisplayBorder/CustomerBorderDisplay.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kassasystemet.Customer
+namespace Kassasystemet.Customer.DisplayBorder
 {
     public class CustomerBorderDisplay
     {
